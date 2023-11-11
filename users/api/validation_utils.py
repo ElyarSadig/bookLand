f178d9d6f504b4dcdc8a20babeba746dbefc738b
elyar <elyarnejati@gmail.com>
2023-11-11 09:47:14 +0330

    updated the names of the functions and added password match function

--- users/api/validation_utils.py
+++ users/api/validation_utils.py
@@ -1,7 +1,7 @@
 import re
 
 
-def validate_username(username):
+def is_username_valid(username):
     # Check if the username contains only letters, numbers, underscores, or hyphens
     if not re.match("^[a-zA-Z0-9_-]*$", username):
         return False
@@ -13,12 +13,26 @@ def validate_username(username):
     return True
 
 
-def validate_email(email):
+def is_email_valid(email):
     # Define a regular expression pattern for a valid email address
     email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
 
     # Check if the email matches the pattern
     if re.match(email_pattern, email):
+        return True
+    else:
+        return False
+
+
+def password_match(password, password2):
+    # additional password validation
+    return password == password2
+
+
+def validate_iranian_phone_number(phone_number):
+    pattern = r'^0\d{10}$'
+
+    if re.match(pattern, phone_number):
         return True
     else:
         return False
