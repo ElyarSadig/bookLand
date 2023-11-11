906f15ec567cf12265b15e3311792974ee6e2739
elyar <elyarnejati@gmail.com>
2023-11-11 09:39:09 +0330

    Added APIResult class

---
+++ users/api/api_result.py
@@ -0,0 +1,11 @@
+
+class APIResult:
+    def __init__(self):
+        self.api_result = {
+            "result": {
+                "error_code": "",
+                "error_message": "",
+                "errors": ""
+            },
+            "data": ""
+        }
