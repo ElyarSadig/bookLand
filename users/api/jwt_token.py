7919578c501381f6c58ed8da70e240a10f08433b
elyar <elyarnejati@gmail.com>
2023-11-12 20:50:47 +0330

    removed decode_jwt_token function

--- users/api/jwt_token.py
+++ users/api/jwt_token.py
@@ -9,21 +9,9 @@ def generate_jwt_token(user_id, role_id):
     token_payload = {
         "user_id": user_id,
         "role_id": role_id,
-        "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=5)
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=4)
     }
 
     token = jwt.encode(token_payload, secret_key, algorithm="HS256")
 
     return token
-
-
-def decode_jwt_token(token, secret_key):
-    try:
-        decoded_token = jwt.decode(token, secret_key, algorithms=["HS256"])
-        return decoded_token
-    except jwt.ExpiredSignatureError:
-        print("Token has expired.")
-        return None
-    except jwt.InvalidTokenError:
-        print("Token is invalid or corrupted.")
-        return None
