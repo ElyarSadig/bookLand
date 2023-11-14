5091cefcd65299757ba21434a9a09fc37dc01619
elyar <elyarnejati@gmail.com>
2023-11-14 21:31:19 +0330

    updated the upload_to_file_server function

--- users/api/file_handler.py
+++ users/api/file_handler.py
@@ -19,16 +19,15 @@ def generate_uuid(file_path):
 
 
 def process_and_upload_identity_path(file_path, postfix="/identities"):
-    check_extension(file_path)
     return upload_to_file_server(file_path, postfix)
 
 
 def process_and_upload_publications_image(file_path, postfix="/publications"):
-    check_extension(file_path)
     return upload_to_file_server(file_path, postfix)
 
 
 def upload_to_file_server(file_path, postfix):
+    check_extension(file_path)
     file_server_url = config('FILE_SERVER_URL')
     auth_token = config('AUTH_TOKEN')
     file_name = generate_uuid(file_path)
