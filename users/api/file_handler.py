c388540f3412e9e2121f3f43ceea4227a0cd0bb0
elyar <elyarnejati@gmail.com>
2023-11-15 09:18:50 +0330

    updated file_handler and fixed some issues

--- users/api/file_handler.py
+++ users/api/file_handler.py
@@ -8,14 +8,14 @@ from .exceptions import InvalidFileFormatError, FileUploadFailedError
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'pdf', 'png'}
 
 
-def check_extension(file_path):
-    file_extension = os.path.splitext(file_path)[1][1:].lower()
-    if file_extension not in ALLOWED_EXTENSIONS:
-        raise InvalidFileFormatError()
+# def check_extension(file_path):
+#     file_extension = os.path.splitext(file_path)[1][1:].lower()
+#     if file_extension not in ALLOWED_EXTENSIONS:
+#         raise InvalidFileFormatError()
 
 
-def generate_uuid(file_path):
-    return f"{uuid.uuid4()}_{os.path.basename(file_path)}"
+def generate_uuid():
+    return str(uuid.uuid4())
 
 
 def process_and_upload_identity_path(file_path, postfix="/identities"):
@@ -26,22 +26,26 @@ def process_and_upload_publications_image(file_path, postfix="/publications"):
     return upload_to_file_server(file_path, postfix)
 
 
-def upload_to_file_server(file_path, postfix):
-    check_extension(file_path)
+def upload_to_file_server(file, postfix):
+    # check_extension(file_path)
+    file_name = generate_uuid() + '_' + file.name
     file_server_url = config('FILE_SERVER_URL')
     auth_token = config('AUTH_TOKEN')
-    file_name = generate_uuid(file_path)
+    files = {'file': (file_name, file)}
 
-    with open(file_path, 'rb') as file:
-        files = {'file': (file_name, file)}
+    response = requests.post(
+        file_server_url + postfix,
+        files=files,
+        headers={'Authorization': auth_token},
+        timeout=1
+    )
 
-        response = requests.post(file_server_url + postfix, files=files, headers={'Authorization': auth_token},
-                                 timeout=0.5)
+    if response.status_code == 200:
+        return file_name  # Adjust this based on your file server response
 
-        if response.status_code == 200:
-            return file_name  # Return the file name
-        else:
-            raise FileUploadFailedError()
+
+
+
 
 
 
