ddd5603f44ac23e46b80341d9b6d525e75e74e09
elyar <elyarnejati@gmail.com>
2023-11-16 20:26:08 +0330

    updated email util functions to render text/html

--- users/api/email_utils.py
+++ users/api/email_utils.py
@@ -1,22 +1,28 @@
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 import threading
-from django.core.mail import EmailMessage
 
 
-def send_email(subject, body, from_email, recipient_list):
-    email = EmailMessage(
+def send_email(subject, from_email, recipient_list, html_content):
+    email = EmailMultiAlternatives(
         subject=subject,
         from_email=from_email,
-        body=body,
         to=recipient_list,
     )
+    email.attach_alternative(html_content, "text/html")
     email.send()
 
 
-def send_email_background_task(activation_code, email):
-    subject = "Email Verification"
-    body = f"Here is your activation code {activation_code}"
+def send_email_background_task(subject, template, code, email):
     from_email = "BookLand@email.com"
     recipient_list = [email]
 
-    email_thread = threading.Thread(target=send_email, args=(subject, body, from_email, recipient_list))
+    context = {'code': code}
+
+    html_content = render_to_string(template, context)
+
+    email_thread = threading.Thread(
+        target=send_email,
+        args=(subject, from_email, recipient_list, html_content)
+    )
     email_thread.start()
