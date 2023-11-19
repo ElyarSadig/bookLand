2628bbc028001919884630f03d0cf644c0a82c93
elyar <elyarnejati@gmail.com>
2023-11-19 13:32:56 +0330

    updated db_utils and fixed some issues

--- accounts/api/db_utils.py
+++ accounts/api/db_utils.py
@@ -3,8 +3,7 @@ from django.db import connection
 
 
 def hash_password(password):
-    hashed_password = hashlib.sha256(password.encode()).hexdigest()
-    return hashed_password
+    return hashlib.sha256(password.encode()).hexdigest()
 
 
 def info_dict(query, user_id):
@@ -12,7 +11,7 @@ def info_dict(query, user_id):
         cursor.execute(query, [user_id])
         results = cursor.fetchall()
         data = [dict(zip([col[0] for col in cursor.description], row)) for row in results]
-    connection.close()
+
     return data
 
 
@@ -27,7 +26,7 @@ class AccountManagementDBUtils:
         with connection.cursor() as cursor:
             cursor.execute(query, [user_id])
             stored_password = cursor.fetchone()[0]
-        connection.close()
+
         return stored_password
 
     @classmethod
@@ -40,67 +39,123 @@ class AccountManagementDBUtils:
                             """
         with connection.cursor() as cursor:
             cursor.execute(query, [hashed_newpassword, user_id])
-        connection.close()
+
 
     @classmethod
     def get_username_email(cls, user_id):
         query = """
-                            SELECT Username, Email FROM public.Users
-                            WHERE (id = %s)
-                                    """
+                SELECT Username, Email FROM public.Users
+                WHERE (id = %s)
+                """
         with connection.cursor() as cursor:
             cursor.execute(query, [user_id])
             username, email = cursor.fetchone()
-        connection.close()
+
         return username, email
 
     @classmethod
     def get_user_bookmarks(cls, user_id):
         query = """
-                            SELECT public.Books.*, UserBookMarks.IsDelete AS bookmark_isdelete 
-                            FROM public.users 
-                            JOIN public.userbookmarks ON users.id = userbookmarks.userid
-                            JOIN public.books on userbookmarks.bookid = books.id
-                            WHERE users.id = %s
-                                            """
+                    SELECT
+                        userbookmarks.id as bookmarkid, 
+                        books.id as bookid,
+                        users.publicationsname as publisher,
+                        books.bookname,
+                        books.authorname,
+                        books.translatorname,
+                        books.releaseddate,
+                        books.bookcoverimage,
+                        books.price,
+                        books.description,
+                        books.numberofpages,
+                        languages.name as language
+                    FROM userbookmarks
+                    JOIN books ON userbookmarks.bookid = books.id
+                    JOIN users ON books.userid = users.id
+                    JOIN languages ON books.languageid = languages.id
+                    WHERE userbookmarks.userid = %s AND userbookmarks.isdelete = false;
+                """
         return info_dict(query=query, user_id=user_id)
 
     @classmethod
     def get_user_books(cls, user_id):
         query = """
-                            SELECT books.* 
-                            FROM books 
-                            JOIN userbooks ON books.id = userbooks.bookid
-                            WHERE userbooks.userid = %s
-                                                    """
+                SELECT 
+                    books.id as bookid,
+                    users.publicationsname as publisher,
+                    books.bookname,
+                    books.authorname,
+                    books.translatorname,
+                    books.releaseddate,
+                    books.bookcoverimage,
+                    books.price,
+                    books.description,
+                    books.numberofpages,
+                    languages.name as language
+                FROM books
+                JOIN userbooks ON books.id = userbooks.bookid
+                JOIN users ON books.userid = users.id
+                JOIN languages ON books.languageid = languages.id
+                WHERE userbooks.userid = %s AND books.isdelete = false;
+                """
         return info_dict(query=query, user_id=user_id)
 
     @classmethod
     def update_user_bookmark(cls, user_id, bookmark_id):
-        try:
-            query = """
-                        Update public.UserBookMarks 
-                        SET IsDelete = TRUE
-                        WHERE UserId = %s AND Id = %s
-                                                    """
-            with connection.cursor() as cursor:
-                cursor.execute(query, [user_id, bookmark_id])
+        query = """
+                Update public.UserBookMarks 
+                SET IsDelete = TRUE
+                WHERE UserId = %s AND Id = %s
+                """
+        with connection.cursor() as cursor:
+            cursor.execute(query, [user_id, bookmark_id])
 
-        except Exception:
-            raise Exception
+    @classmethod
+    def get_total_successful_amount(cls, user_id):
+        query = """
+            SELECT SUM(Amount) AS total_amount
+            FROM public.WalletActions
+            WHERE UserId = %s AND IsSuccessful = TRUE;
+        """
+
+        with connection.cursor() as cursor:
+            cursor.execute(query, [user_id])
+            result = cursor.fetchone()
+
+        total_amount = result[0] if result and result[0] is not None else 0.0
+
+        return total_amount
 
     @classmethod
     def get_user_wallet_history(cls, self, user_id, request):
         query = """
-                            SELECT * 
-                            FROM walletactions 
-                            WHERE userid = %s
-                                                            """
+                SELECT
+                     walletactions.id,
+                     walletactiontypes.actiontype as actiontype,
+                     walletactions.amount,
+                     walletactions.issuccessful,
+                     walletactions.description,
+                     walletactions.createddate
+                FROM walletactions 
+                JOIN walletactiontypes ON walletactiontypes.id = walletactions.actiontypeid
+                WHERE userid = %s
+                """
         with connection.cursor() as cursor:
             cursor.execute(query, [user_id])
             results = cursor.fetchall()
 
             paginator = self.pagination_class()
             paginated_results = paginator.paginate_queryset(results, request)
-        connection.close()
-        return [dict(zip([col[0] for col in cursor.description], row)) for row in paginated_results]
+
+            total_pages = paginator.page.paginator.num_pages
+
+            current_page = paginator.page.number
+
+            data = {
+                "page_size": 9,
+                "page_index": current_page,
+                "count": total_pages,
+                "data": [dict(zip([col[0] for col in cursor.description], row)) for row in paginated_results]
+            }
+
+        return data
