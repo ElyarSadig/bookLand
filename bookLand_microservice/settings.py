91a65e2c772c88689bf07620d7b529bfed300b17
elyar <elyarnejati@gmail.com>
2023-11-24 09:49:02 +0330

    Removed the LOGGING from settings

--- bookLand_microservice/settings.py
+++ bookLand_microservice/settings.py
@@ -28,19 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-        },
-    },
-    "root": {
-        "handlers": ["console"],
-        "level": "WARNING",
-    },
-}
 
 # trusted domains
 CORS_ORIGIN_WHITELIST = (
