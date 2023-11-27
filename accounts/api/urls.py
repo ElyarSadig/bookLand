2be347a4cc1fd7d3f80c629b77b067de1a747f53
elyar <elyarnejati@gmail.com>
2023-11-27 22:07:02 +0330

    added names to urls

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -4,10 +4,10 @@ from .views import ChangePasswordView, UserProfileView, GetUserBooksView, \
 
 urlpatterns = [
     path("change-password", ChangePasswordView.as_view(), name='change-old-password'),
-    path("profile", UserProfileView.as_view()),
-    path("books", GetUserBooksView.as_view()),
-    path("bookmarks", GetUserBookMarksView.as_view()),
-    path("wallet-history", GetUserWalletHistoryView.as_view()),
-    path("bookmarks/<str:bookmark_id>", DeleteUserBookMarkView.as_view()),
-    path("wallet-balance", UserWalletBalance.as_view())
+    path("profile", UserProfileView.as_view(), name='user-profile'),
+    path("books", GetUserBooksView.as_view(), name='user-books'),
+    path("bookmarks", GetUserBookMarksView.as_view(), name='user-bookmarks'),
+    path("wallet-history", GetUserWalletHistoryView.as_view(), name='user-wallet-history'),
+    path("bookmarks/<str:bookmark_id>", DeleteUserBookMarkView.as_view(), name='delete-user-bookmark'),
+    path("wallet-balance", UserWalletBalance.as_view(), name='user-wallet-balance')
 ]
