06ba30a16315b7795784460ee962d40e9d6efd2f
elyar <elyarnejati@gmail.com>
2023-11-30 11:42:54 +0330

    added new url endpoints

--- books/api/urls.py
+++ books/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from books.api.views import BookDetailView
+from books.api.views import BookDetailView, BookReviewsView, BookCategoriesView
 
 
 urlpatterns = [
-    path('<str:book_id>', BookDetailView.as_view(), name="book-detail")
+    path('<str:book_id>', BookDetailView.as_view(), name="book-detail"),
+    path('<str:book_id>/reviews', BookReviewsView.as_view(), name='book-reviews'),
+    path('<str:book_id>/categories', BookCategoriesView.as_view(), name='book-categories')
 ]
