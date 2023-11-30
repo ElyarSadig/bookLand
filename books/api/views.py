efd3cf26c6fe3d6cb5e45344f685e08e0e2ebfaa
elyar <elyarnejati@gmail.com>
2023-11-30 11:43:10 +0330

    added new views for new endpoints

--- books/api/views.py
+++ books/api/views.py
@@ -18,3 +18,33 @@ class BookDetailView(GenericAPIView):
         response.api_result['data'] = data
 
         return Response(response.api_result, status=status.HTTP_200_OK)
+
+
+class BookReviewsView(GenericAPIView):
+
+    def get(self, request, book_id, *args, **kwargs):
+        response = APIResult()
+
+        data = BookManagementDBUtils.get_book_review_counts(book_id)
+
+        if len(data) == 0:
+            return Response(response.api_result, status=status.HTTP_404_NOT_FOUND)
+
+        response.api_result['data'] = data
+
+        return Response(response.api_result, status=status.HTTP_200_OK)
+
+
+class BookCategoriesView(GenericAPIView):
+
+    def get(self, request, book_id, *args, **kwargs):
+        response = APIResult()
+
+        categories = BookManagementDBUtils.get_book_categories(book_id)
+
+        if len(categories) == 0:
+            return Response(response.api_result, status=status.HTTP_404_NOT_FOUND)
+
+        response.api_result['data'] = [category[0] for category in categories]
+
+        return Response(response.api_result, status=status.HTTP_200_OK)
