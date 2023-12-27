8aa8ffbafc71d80e34e5d1330cd6442abf999746
fereshteh <salmanifereshteh@gmail.com>
2023-12-27 11:52:17 +0330

    add publisher profile logic with endpoints

---
+++ publishers/api/jwt_auth.py
@@ -0,0 +1,43 @@
+import jwt
+from decouple import config
+from functools import wraps
+from accounts.api.exceptions import *
+
+
+def extract_user_and_role_id(token):
+    secret_key = config("JWT_SECRET_KEY")
+    try:
+        decoded_token = jwt.decode(token, secret_key, algorithms=["HS256"])
+        user_id = decoded_token.get("user_id")
+        role_id = decoded_token.get("role_id")
+        return user_id, role_id
+
+    except jwt.ExpiredSignatureError:
+        raise ExpiredSignatureError()
+
+    except jwt.InvalidTokenError:
+        raise InvalidTokenError()
+
+    except Exception:
+        raise Exception()
+
+
+def login_required(view_func):
+    @wraps(view_func)
+    def wrapper(self, request, *args, **kwargs):
+        auth_header = request.META.get('HTTP_AUTHORIZATION')
+
+        if auth_header and auth_header.startswith('Bearer '):
+            token = auth_header.split('Bearer ')[1]
+
+            user_id, role_id = extract_user_and_role_id(token)
+
+            if role_id != 1:
+                raise PermissionDeniedError()
+
+            return view_func(self, request, user_id, role_id, *args, **kwargs)
+
+        else:
+            raise MissingTokenError()
+
+    return wrapper
