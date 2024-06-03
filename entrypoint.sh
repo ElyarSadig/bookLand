d546fae4f332dbef3aa6ff85402977ecf7bf4c50
elyar <elyarnejati@gmail.com>
2024-06-03 13:57:45 +0330

    update: dockerization & add entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Applying database migrations..."
+python manage.py migrate
+
+echo "Seeding database..."
+python manage.py seed
+
+echo "Starting Gunicorn..."
+exec gunicorn --bind 0.0.0.0:8000 bookLand_microservice.wsgi:application
