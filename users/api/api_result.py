482a1f8ac154400c3719fe4d5bba417291ec6723
elyar <elyarnejati@gmail.com>
2024-06-04 13:27:23 +0330

    update: remove error_code from api result

--- users/api/api_result.py
+++ users/api/api_result.py
@@ -3,7 +3,6 @@ class APIResult:
     def __init__(self):
         self.api_result = {
             "result": {
-                "error_code": "",
                 "error_message": "",
                 "errors": ""
             },
