54928cd2aedf0eaf1f7050030c644f1ece9e0b10
elyar <elyarnejati@gmail.com>
2024-06-04 13:35:10 +0330

    update: update books views with ORM

--- books/api/views.py
+++ books/api/views.py
@@ -1,40 +1,38 @@
 from rest_framework.generics import GenericAPIView
-from books.api.db_utils import BookManagementDBUtils
 from users.api.api_result import APIResult
 from rest_framework.response import Response
 from rest_framework import status
 from accounts.api.jwt_auth import login_required
+from books.models import Category, Language, UserBook, Book
+from books.api.serializers import CategorySerializer, LanguageSerializer, BookDetailSerializer, BookReviewsSerializer
 
 
 class BookDetailView(GenericAPIView):
-
+    serializer_class = BookDetailSerializer
     def get(self, request, book_id, *args, **kwargs):
         response = APIResult()
-
-        data = BookManagementDBUtils.get_book_detail(book_id)
-
-        if len(data) == 0:
+        book = Book.objects.filter(id=book_id, is_delete=False).prefetch_related('categories').first()
+        if not book:
             return Response(response.api_result, status=status.HTTP_404_NOT_FOUND)
-
-        response.api_result['data'] = data
-
+        serializer = BookDetailSerializer(book)
+        response.api_result["data"] = serializer.data
         return Response(response.api_result, status=status.HTTP_200_OK)
 
 
 class BookReviewsView(GenericAPIView):
+    serializer_class = BookReviewsSerializer
 
     def get(self, request, book_id, *args, **kwargs):
         response = APIResult()
 
-        data = BookManagementDBUtils.get_book_review_counts(book_id)
+        book = Book.objects.filter(id=book_id, is_delete=False).first()
 
-        if len(data) == 0:
-            response.api_result['data'] = [{
-                "reviewaverage": 0,
-                "reviewcount": 0
-            }]
-        else:
-            response.api_result['data'] = data
+        if not book:
+            response.api_result['data'] = None
+            return Response(response.api_result, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.get_serializer(book)
+        response.api_result['data'] = serializer.data
 
         return Response(response.api_result, status=status.HTTP_200_OK)
 
@@ -43,14 +41,9 @@ class BookCategoriesView(GenericAPIView):
 
     def get(self, request, book_id, *args, **kwargs):
         response = APIResult()
-
-        categories = BookManagementDBUtils.get_book_categories(book_id)
-
-        if len(categories) == 0:
-            return Response(response.api_result, status=status.HTTP_404_NOT_FOUND)
-
-        response.api_result['data'] = [category[0] for category in categories]
-
+        categories = Category.objects.filter(book_categories__book_id=book_id)
+        serializer = CategorySerializer(categories, many=True)
+        response.api_result['data'] = serializer.data
         return Response(response.api_result, status=status.HTTP_200_OK)
 
 
@@ -59,28 +52,20 @@ class OriginalBookFileView(GenericAPIView):
     @login_required
     def get(self, request, user_id, role_id, *args, **kwargs):
         response = APIResult()
-
         book_id = kwargs.get('book_id')
-
-        if not BookManagementDBUtils.has_user_bought_book(user_id, book_id):
+        if not UserBook.has_user_bought_book(user_id, book_id):
             return Response(response.api_result, status=status.HTTP_404_NOT_FOUND)
-
-        original_book_file_path = BookManagementDBUtils.get_original_book_filepath(book_id)
-
-        response.api_result['data'] = original_book_file_path
-
+        book = Book.objects.get(id=book_id)
+        response.api_result['data'] = book.original_file
         return Response(response.api_result, status=status.HTTP_200_OK)
 
 
 class CategoriesView(GenericAPIView):
-
     def get(self, request, *args, **kwargs):
         response = APIResult()
-
-        categories = BookManagementDBUtils.get_all_categories()
-
-        response.api_result['data'] = categories
-
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        response.api_result['data'] = serializer.data
         return Response(response.api_result, status=status.HTTP_200_OK)
 
 
@@ -88,9 +73,7 @@ class LanguagesView(GenericAPIView):
 
     def get(self, request, *args, **kwargs):
         response = APIResult()
-
-        languages = BookManagementDBUtils.get_all_languages()
-
-        response.api_result['data'] = languages
-
+        languages = Language.objects.all()
+        serializer = LanguageSerializer(languages, many=True)
+        response.api_result['data'] = serializer.data
         return Response(response.api_result, status=status.HTTP_200_OK)
